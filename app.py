7e6c6b07d215ea931f3a845a344b8ea7d3284547
Hello-kath <vasquezdianakatherine.com>
2025-04-02 13:21:14 -0500

    configurar para subir a render 4

--- app.py
+++ app.py
@@ -15,3 +15,6 @@ app.config.from_object(Config)
 app.register_blueprint(contactosRuta, url_prefix='/contactos')
 app.register_blueprint(buscarContR, url_prefix='/buscar')
 
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port)
